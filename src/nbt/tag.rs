51e79c87810543b260e8d8013d818f1961bbc429
Qther <qther@tuta.io>
2026-07-07 12:21:52 +0800

    fix merge issues

--- src/nbt/tag.rs
+++ src/nbt/tag.rs
@@ -5,12 +5,15 @@ use crab_nbt::nbt::utils::*;
 use derive_more::From;
 use std::fmt::{self, Debug, Display, Formatter};
 use std::io::Cursor;
-use std::mem::{Discriminant, discriminant};
+use std::mem::{discriminant, Discriminant};
 use std::str::FromStr;
 
-use crate::nbt::snbt::de::numbers::{may_start_number, read_number_or_numboid_const};
-use crate::nbt::snbt::de::utils::{FromVisitor, StrVisitor, consume_whitespace, expect_char, expect_str, impl_FromStr_through_FromVisitor, read_slice_while, read_string};
 use crate::nbt::error::SnbtDeserialisationError;
+use crate::nbt::snbt::de::numbers::{may_start_number, read_number_or_numboid_const};
+use crate::nbt::snbt::de::utils::{
+    consume_whitespace, expect_char, expect_str, impl_FromStr_through_FromVisitor,
+    read_slice_while, read_string, FromVisitor, StrVisitor,
+};
 
 use crate::nbt::list::NbtList;
 
@@ -278,7 +281,7 @@ impl NbtTag {
             LongArray(x) => !x.is_empty(),
             String(x) => !x.is_empty(),
             List(x) => !x.is_empty(),
-            Compound(x) => !x.child_tags.is_empty()
+            Compound(x) => !x.child_tags.is_empty(),
         }
     }
 }
@@ -329,34 +332,43 @@ impl FromVisitor for NbtTag {
         const TRUE: NbtTag = NbtTag::Byte(1);
         const FALSE: NbtTag = NbtTag::Byte(0);
 
-        match visitor.peek().ok_or_else(
-            || SnbtDeserialisationError::from_visitor(visitor, "any SNBT character")
-        )? {
+        match visitor
+            .peek()
+            .ok_or_else(|| SnbtDeserialisationError::from_visitor(visitor, "any SNBT character"))?
+        {
             '{' => NbtCompound::from_visitor(visitor).map(NbtTag::Compound),
             '[' => {
                 _ = visitor.next();
                 consume_whitespace(visitor);
                 if visitor.next_if(|c| c == ']').is_some() {
-                    Ok(NbtTag::List(vec![]))
-                } else if visitor.as_str().chars().nth(1).filter(|c| *c == ';').is_some() {
+                    Ok(NbtTag::List(NbtList::new()))
+                } else if visitor
+                    .as_str()
+                    .chars()
+                    .nth(1)
+                    .filter(|c| *c == ';')
+                    .is_some()
+                {
                     // we know visitor.nth(1) will be Some and StrVisitor is fused
                     // => visitor.next must be Some
                     match visitor.next().unwrap() {
                         'I' => read_snbt_array::<i32>(visitor).map(NbtTag::IntArray),
-                        'B' => read_snbt_array::<i8>(visitor).map(
-                            |arr| NbtTag::ByteArray(arr.into_iter().map(|b| b as u8).collect())
-                        ),
+                        'B' => read_snbt_array::<i8>(visitor).map(|arr| {
+                            NbtTag::ByteArray(arr.into_iter().map(|b| b as u8).collect())
+                        }),
                         'L' => read_snbt_array::<i64>(visitor).map(NbtTag::LongArray),
-                        _ => return Err(SnbtDeserialisationError::from_visitor(
-                            visitor,
-                            "an array type identifier"
-                        ))
+                        _ => {
+                            return Err(SnbtDeserialisationError::from_visitor(
+                                visitor,
+                                "an array type identifier",
+                            ))
+                        }
                     }
                 } else {
                     // NOTE: This branch also triggers if visitor is fully consumed
-                    read_list(visitor).map(NbtTag::List)
+                    read_list(visitor).map(|v| NbtTag::List(NbtList::from_iter(v)))
                 }
-            },
+            }
             c if may_start_number(c) => read_number_or_numboid_const(visitor),
             _ => {
                 if expect_str(visitor, "true").is_ok() {
@@ -365,11 +377,11 @@ impl FromVisitor for NbtTag {
                     Ok(FALSE)
                 } else if expect_str(visitor, "bool(").is_ok() {
                     consume_whitespace(visitor);
-                    
+
                     let tag = if visitor.peek().is_some() {
                         read_number_or_numboid_const(visitor)?.is_truthy().into()
                     } else {
-                        return Err(SnbtDeserialisationError::from_visitor(visitor, "EOF"))
+                        return Err(SnbtDeserialisationError::from_visitor(visitor, "EOF"));
                     };
                     // todo!("Read number or read true/false");
                     consume_whitespace(visitor);
@@ -377,9 +389,8 @@ impl FromVisitor for NbtTag {
                     Ok(tag)
                 } else if expect_str(visitor, "uuid(").is_ok() {
                     consume_whitespace(visitor);
-                    let tag = uuid_from_str(&read_string(visitor)?)
-                        .map(NbtTag::IntArray);
-                    
+                    let tag = uuid_from_str(&read_string(visitor)?).map(NbtTag::IntArray);
+
                     consume_whitespace(visitor);
                     expect_char(visitor, ')', ")")?;
                     tag
@@ -413,10 +424,10 @@ fn uuid_from_str(s: &str) -> Result<Vec<i32>, SnbtDeserialisationError> {
     type Out = i32;
 
     let mut bytes = [0u8; UUID_V4_SIZE];
-    for (i, res) in s.split('-')
+    for (i, res) in s
+        .split('-')
         .flat_map(|substr| {
-            (0..(substr.len() / 2))
-                .map(|i| &substr[2*i..(2*(i+1)).min(substr.len())])
+            (0..(substr.len() / 2)).map(|i| &substr[2 * i..(2 * (i + 1)).min(substr.len())])
         })
         .map(|s| u8::from_str_radix(s, 16))
         .enumerate()
@@ -431,21 +442,20 @@ fn uuid_from_str(s: &str) -> Result<Vec<i32>, SnbtDeserialisationError> {
     // RangeIndexing an array gives a slice, whose size is unknown at compile time.
     // i32::from_be_bytes requires moving an array into it to work,
     // so we would either have to copy the array or use a different method (of which I am not aware)
-    // 
+    //
     // SAFETY:
     //  Arrays are purely groups of data whose size is determined at compile time.
     //  More specifically, an array [T;N]'s size is determined by size_of::<T>()*N
     //      and an element arr[i] is offset by i*size_of::<T>() bytes.
     //  This means that [[T;N];M] has the same layout as a [T;N*M] with respect to its values.
     //  Ergo, transmuting here is safe, because we don't change the bounds of any elements.
-    let parts: [[u8; size_of::<Out>()]; UUID_V4_SIZE/size_of::<Out>()] = unsafe { std::mem::transmute(bytes) };
-    Ok(
-        parts.into_iter()
-            .map(|int_bytes| Out::from_be_bytes(int_bytes))
-            .collect()
-    )
+    let parts: [[u8; size_of::<Out>()]; UUID_V4_SIZE / size_of::<Out>()] =
+        unsafe { std::mem::transmute(bytes) };
+    Ok(parts
+        .into_iter()
+        .map(|int_bytes| Out::from_be_bytes(int_bytes))
+        .collect())
 }
-
 
 const LIST_SEPARATOR_MSG: &'static str = ", or ]";
 /// Reads an SNBT List with correction for heterogeneous lists.
@@ -456,34 +466,42 @@ fn read_list(visitor: &mut StrVisitor) -> Result<Vec<NbtTag>, SnbtDeserialisatio
     loop {
         consume_whitespace(visitor);
         let tag = NbtTag::from_visitor(visitor)?;
-        homogeneous_content_type = homogeneous_content_type.map_or(
-            Some(Some(discriminant(&tag))),
-            |list_content| 
-            Some(list_content.filter(|d| *d == discriminant(&tag)))
-        );
+        homogeneous_content_type = homogeneous_content_type
+            .map_or(Some(Some(discriminant(&tag))), |list_content| {
+                Some(list_content.filter(|d| *d == discriminant(&tag)))
+            });
         content.push(tag);
 
         consume_whitespace(visitor);
 
-        if visitor.next_if(|c| c == ',' || c == ']')
-            .ok_or_else(||
-                SnbtDeserialisationError::from_visitor(visitor, LIST_SEPARATOR_MSG)
-            )? == ']'
+        if visitor
+            .next_if(|c| c == ',' || c == ']')
+            .ok_or_else(|| SnbtDeserialisationError::from_visitor(visitor, LIST_SEPARATOR_MSG))?
+            == ']'
         {
-            break
+            break;
         }
     }
-    if homogeneous_content_type.unwrap_or(Some(discriminant(&NbtTag::End))).is_none() {
+    if homogeneous_content_type
+        .unwrap_or(Some(discriminant(&NbtTag::End)))
+        .is_none()
+    {
         // Heterogeneous List correction
         // Nbt does not allow hetereogeneous lists
         // (lists where each element may have a different type),
         // but SNBT does (since https://www.minecraft.net/en-us/article/minecraft-snapshot-25w09a).
-        // 
+        //
         // Heterogenous lists must be deserialised into lists of type NbtCompound,
         // with each element contained in a compound like so {"": element}
-        content = content.into_iter().map(|elem| {
-            [(String::new(), elem)].into_iter().collect::<NbtCompound>().into()
-        }).collect();
+        content = content
+            .into_iter()
+            .map(|elem| {
+                [(String::new(), elem)]
+                    .into_iter()
+                    .collect::<NbtCompound>()
+                    .into()
+            })
+            .collect();
     } else {
         content.shrink_to_fit();
     }
@@ -493,11 +511,12 @@ fn read_list(visitor: &mut StrVisitor) -> Result<Vec<NbtTag>, SnbtDeserialisatio
 /// Reads an SNBT array of type Number,
 /// assuming the array identifier (e.g. `[I;`) has already been consumed, save for the semicolon.
 fn read_snbt_array<Number>(
-    visitor: &mut StrVisitor
+    visitor: &mut StrVisitor,
 ) -> Result<Vec<Number>, SnbtDeserialisationError>
-    where Number: FromStr,
-        // TODO: Remove me after better error structures
-        Number::Err: std::error::Error
+where
+    Number: FromStr,
+    // TODO: Remove me after better error structures
+    Number::Err: std::error::Error,
 {
     consume_whitespace(visitor);
     // This was missing once. It took me days to find that bug!
@@ -508,14 +527,16 @@ fn read_snbt_array<Number>(
         content.push(
             read_slice_while(visitor, |c| c.is_ascii_digit() || c == '-')
                 .parse()
-                .expect("todo: better error structures")
+                .expect("todo: better error structures"),
         );
 
         consume_whitespace(visitor);
-        if visitor.next_if(|c| c == ',' || c == ']').ok_or_else(
-            || SnbtDeserialisationError::from_visitor(visitor, LIST_SEPARATOR_MSG)
-        )? == ']' {
-            break
+        if visitor
+            .next_if(|c| c == ',' || c == ']')
+            .ok_or_else(|| SnbtDeserialisationError::from_visitor(visitor, LIST_SEPARATOR_MSG))?
+            == ']'
+        {
+            break;
         }
     }
     Ok(content)
@@ -523,7 +544,10 @@ fn read_snbt_array<Number>(
 
 #[cfg(test)]
 mod tests {
-    use crate::{NbtTag, nbt::{snbt::de::utils::StrVisitor, tag::read_number_or_numboid_const}};
+    use crate::{
+        nbt::{snbt::de::utils::StrVisitor, tag::read_number_or_numboid_const},
+        NbtTag,
+    };
 
     fn number_helper(s: &str) -> NbtTag {
         read_number_or_numboid_const(&mut StrVisitor::new(s)).unwrap()
